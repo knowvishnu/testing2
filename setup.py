9169ecb71e6736cd3fbb47b297b3055dfdc858f7
Vishnu Agarwal <vishnu.agarwal@peak.ai>
2025-07-17 11:57:52 +0530

    Add setup.py for basic Python project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testing2',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='vishnu.agarwal',
+    description='A basic Python project',
+)
